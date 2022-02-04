4818e7e6f5918f870280cebc85d3a52f4884e846
siljp24 <silvia_juarez_24@hotmail.com>
2022-02-04 19:47:02 +0100

    make it better

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const values = require('./values');
 
 const config = {
     database:{
-        url:'mongodb://localhost/PFM',
+        url:'mongodb+srv://Siljp:claveCluster1234@clusterreserva.yzjrv.mongodb.net/Reserva?retryWrites=true&w=majority',
     },
     jwt:{
         secret: '613b92e493bc7362a0b7ce76',
